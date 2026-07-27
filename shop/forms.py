6bc7b9f092c00fc4d4c112306b9178547bd60f8e
Pranay_Khairnar <khairnarpranay89@gmail.com>
2026-07-27 20:47:50 +0530

    shop profile form + validation

--- shop/forms.py
+++ shop/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
 from .models import Shop
+from accounts.validators import allow_only_images_validator
 
 class ShopForm(forms.ModelForm):
+    shop_license=forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}),validators=[allow_only_images_validator])
     class Meta:
         model=Shop
         fields=['owner_name', 'shop_license']
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import ShopForm
+from accounts.forms import UserProfileForm
+from accounts.models import UserProfile
+from .models import Shop
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
+from accounts.views import check_role_shopper
 
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_shopper)
 def shopprofile(request):
-    return render(request,'shop/shopprofile.html')
+    profile=get_object_or_404(UserProfile,user=request.user)
+    shop=get_object_or_404(Shop,user=request.user)
+
+    if request.method=='POST':
+        profile_form = UserProfileForm(request.POST,request.FILES,instance=profile)
+        shop_form=ShopForm(request.POST,request.FILES,instance=shop)
+        if profile_form.is_valid() and shop_form.is_valid():
+            profile_form.save()
+            shop_form.save()
+            messages.success(request,'Settings updated.')
+            return redirect('shopprofile')
+        else:
+            print(profile_form.errors)
+            print(shop_form.errors)
+    else:
+        profile_form=UserProfileForm(instance=profile)
+        shop_form=ShopForm(instance=shop)
+
+
+    
+
+    context={
+        'profile_form':profile_form,
+        'shop_form':shop_form,
+        'profile':profile,
+        'shop':shop,
+
+    }
+    return render(request,'shop/shopprofile.html',context)
